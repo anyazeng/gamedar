642ac1f10c1eb30d30c1e53a8d394e8ace2781b0
Anya Zeng <anya.tseng@gmail.com>
2025-01-17 15:31:19 +1100

    feat: build responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "side main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="side" bg="teal">
+          side
+        </GridItem>
+      </Show>
 
-  return <Button colorScheme="teal">Clcick</Button>;
+      <GridItem area="main" bg="gold">
+        main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
